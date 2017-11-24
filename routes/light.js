640efd658ee8d9126674b92516229f72d427275f
Juan J. Sierralta <sierralta@gmail.com>
2017-11-23 22:06:22 -0300

    Minor clean

--- routes/light.js
+++ routes/light.js
@@ -77,7 +77,7 @@ module.exports = (app, yeelightSearch) => {
     var yeelight = yeelightSearch.getYeelightById(req.params.light);
 
     if (yeelight) {
-      light = {
+      let light = {
         id: yeelight.id,
         name: yeelight.name,
         ct: yeelight.ct,
