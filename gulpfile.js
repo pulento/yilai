f1fec2942786910d3354f6cc836d60bb988f3f44
Juan J. Sierralta <sierralta@gmail.com>
2017-11-27 23:36:33 -0300

    Polishing gulfile for SonarQube

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,9 @@ gulp.task("default", function(callback) {
       token: "b8c8270f28cb092a118b634f39e046f026ee0a04",
       options: {
         "sonar.organization": "pulento-github",
-        "sonar.javascript.lcov.reportPaths": "client/coverage/lcov.info"
+        "sonar.javascript.lcov.reportPaths": "client/coverage/lcov.info",
+        "sonar.exclusions": "client/coverage/*",
+        "sonar.coverage.exclusions": "**/tests/**"
       }
     },
     callback
